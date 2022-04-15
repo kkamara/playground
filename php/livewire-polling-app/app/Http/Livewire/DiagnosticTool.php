15a4612059624bd99313c9be51d8291d0bf7b409
kkamara <kelvinkamara@protonmail.com>
2022-04-15 11:42:39 +0100

    Ping for response time

--- php/livewire-polling-app/app/Http/Livewire/DiagnosticTool.php
+++ php/livewire-polling-app/app/Http/Livewire/DiagnosticTool.php
@@ -7,11 +7,26 @@ use Livewire\Component;
 class DiagnosticTool extends Component
 {
     public bool $active = false;
+    public string $message = '';
 
-    protected $listeners = ['toggleActive',];
+    protected $listeners = [
+        'toggleActive',
+        'ping-time' => 'getPingDuration', 
+        'ping-sent' => 'replyToClient',
+    ];
 
     public function toggleActive(): void {
         $this->active = !$this->active;
+    }
+
+    public function replyToClient($sentTime)
+    {
+        $this->emit("ping-reply", $sentTime);
+    }
+
+    public function getPingDuration($difference)
+    {
+        $this->message = "The request-response time was $difference milliseconds";
     }
 
     public function render()
--- php/livewire-polling-app/resources/views/livewire/diagnostic_tool.blade.php
+++ php/livewire-polling-app/resources/views/livewire/diagnostic_tool.blade.php
@@ -1,8 +1,8 @@
-<div class="col-md-6 offset-md-3">
+<div class='col-md-6 offset-md-3'>
     <button 
-        wire:click="$emit('toggleActive')"
-        wire:loading.attr="disabled" 
-        wire:click="diagnose"
+        wire:click='$emit("toggleActive")'
+        wire:loading.attr='disabled' 
+        wire:click='diagnose'
     >
         Diagnose
     </button>
@@ -13,5 +13,27 @@
         <div wire:poll.1s>
             Current time: {{ now()->toISOString() }}
         </div>
+
+        <button 
+            onclick='sendEvent()'
+            style='border: 1px solid gray; padding: 10px 20px;'
+        >
+            Ping
+        </button>
+
+        <p>{{ $message }}</p>
     @endif
 </div>
+<script>
+    // This function emits an event to the server with the emit-time as a parameter
+    function sendEvent(){
+         Livewire.emit('ping-sent',  Date.now())
+    }
+
+    // This is the client listener waiting for the ping-reply from the server
+    // then calculate the difference and emits an event with the duration of the ping to the server
+    Livewire.on('ping-reply', sentTime => {
+        const replyTime = Date.now()
+        Livewire.emit('ping-time',  replyTime - sentTime)
+    })
+</script>
