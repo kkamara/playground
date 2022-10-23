c2bdc317dc25c712914f3f9cf32aa07a9c870356
kkamara <kelvinkamara@protonmail.com>
2022-10-23 21:08:18 +0100

    Add iterator.js

---
+++ js/iterator.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+/**
+ * @author Kelvin Kamara <www.kelvinkamara.com>
+ */
+
+const myIterable = {};
+myIterable[Symbol.iterator] = function* () {
+  yield 1;
+  yield 2;
+  yield 3;
+};
+
+[...myIterable]; // [1, 2, 3]
+
+class Foo {
+  *[Symbol.iterator]() {
+    yield 1;
+    yield 2;
+    yield 3;
+  }
+}
+
+const someObj = {
+  *[Symbol.iterator]() {
+    yield "a";
+    yield "b";
+  },
+};
+
+console.log(...new Foo()); // 1, 2, 3
+console.log(...someObj); // 'a', 'b'
+
