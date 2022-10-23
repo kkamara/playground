4adfe55c2404de72ee8014678ef7255401d916e7
kkamara <kelvinkamara@protonmail.com>
2022-10-23 18:08:38 +0100

    Add set.js

---
+++ js/set.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const a = ['apple','pear','mango',]
+let x = new Set(a)
+
+x.add('orange')
+
+console.log(x)
+console.log(x.has('orange'))
+
+x.delete('mango')
+
+x.forEach(value => console.log(`>\t${value}`))
+
+/**
+  Set(4) { 'apple', 'pear', 'mango', 'orange' }
+  true
+  >	apple
+  >	pear
+  >	orange
+*/
