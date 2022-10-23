f10d777fc87e4c45659c6e1df71f03e397fa2ebf
kkamara <kelvinkamara@protonmail.com>
2022-10-23 15:42:32 +0100

    Add reduce.js

---
+++ js/reduce.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+/**
+ * @author Kelvin Kamara <www.kelvinkamara.com>
+ */
+let sum = 0
+const arr = [3,7,1,6,3]
+
+sum = arr.reduce((acc, cur) => acc += cur,0)
+
+console.log(sum)
+
+const obj = [{
+  firstName: 'John',
+  lastName: 'Doe',
+}]
+const newObj = obj.reduce((acc, cur) => {
+  if (!acc.name) {
+    acc.name = ''
+  }
+  if (cur.firstName === 'John' && cur.lastName === 'Doe') {
+    acc.name = cur.firstName + ' ' + cur.lastName
+  }
+  return acc
+}, {}) 
+
+console.log(newObj)
+
+// 20
+// { name: 'John Doe' }
+
+
