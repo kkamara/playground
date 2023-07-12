9a52dc296acea5097375692c7d320bbc2eee0394
kkamara <kamaracomputers@gmail.com>
2023-07-12 14:39:52 +0100

    Update learning-folder

---
+++ py/learning-folder/tenth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+def exampleFunc():
+  '''
+  This is my example function.
+  '''
+  pass
+
+'''
+This is my greetings function.
+'''
+def greetingsFunction(str1=None):
+  '''
+  We write `in here` to standard output.
+  '''
+  #Parameters:
+  #    str1 (str):The string which is to be reversed.
+
+  #Returns:
+  #    void:We just print things in greetingsFunction. 
+  print('in here')
+
+if __name__ == '__main__':
+  greetingsFunction()
