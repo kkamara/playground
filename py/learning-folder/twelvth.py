30caf049b6d32595adeb64f053589a8b091bcf27
kkamara <kamaracomputers@gmail.com>
2023-07-12 15:35:29 +0100

    Update learning-folder

---
+++ py/learning-folder/twelvth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+def exampleFunc():
+  '''
+  This is my example function.
+  '''
+  pass
+
+'''
+This is my greetings function.
+'''
+def greetingsFunction(name, age, *names):
+  '''
+  We write things to standard output.
+  Parameters:
+  name (str)
+  age (str)
+  names (*names)
+  '''
+
+  #Returns:
+  #    void:We just print things in greetingsFunction. 
+  if len(names) > 0:
+    # print('Welcome' + name + str(names) + '. You are', str(age), 'years old.')
+    print('Welcome', name, str(names) + '. You are', str(age), 'years old.')
+    return None
+  print('Welcome', name + '. You are', str(age), 'years old.')
+
+def myFunction():
+  '''
+    myFunction
+  '''
+  '''
+  Returns:
+  int:The result of 5 + 4
+  '''
+  return 5 + 4
+
+if __name__ == '__main__':
+  greetingsFunction('Kel', 27, 'www.kelvinkamara.com', 'Kelvin Kamara')
+  print(myFunction(), type(myFunction()))
