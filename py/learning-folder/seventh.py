fad98717d482cd75262021703765da8eef270262
kkamara <kamaracomputers@gmail.com>
2023-07-12 13:44:56 +0100

    Add  to learning folder

---
+++ py/learning-folder/seventh.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+list1 = [1, 2, 3, 4, 5]
+list2 = ['banana', 'apples', 'mango', 'oranges',]
+list2.insert(1, 'cherry')
+print(len(list2), list2)
+print(list2)
+list2.remove('banana')
+print(list2)
+list2.clear()
+print(list2)
