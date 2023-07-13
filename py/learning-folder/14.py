d47a71d1e0e6b178e211f9ec043f56e874836d29
kkamara <kamaracomputers@gmail.com>
2023-07-13 20:15:55 +0100

    Update learning-folder

---
+++ py/learning-folder/14.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+myDict = {
+  'name': 'Tim',
+  'nationality': 'African',
+  'age': 87,
+  'isTall': True,
+  'Qualification': 'College',
+  'friends': ['Peter', 'Paul', 'Precious']
+}
+
+x = myDict
+
+print(x)
