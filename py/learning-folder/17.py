f084db83d3d14b578d59dcbd39c1a113f07e6a47
kkamara <kamaracomputers@gmail.com>
2023-07-13 22:35:41 +0100

    Add to learning-folder

---
+++ py/learning-folder/17.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# 2D Lists
+myList = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(myList[0][0])
+
+for lists in myList:
+  for row in lists:
+    print(row)
---
+++ py/learning-folder/18.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# print('hello')
+print('hello') # This line prints hello.
+print(1)
+'''
+def myFunc():
+    print('hi')
+'''
+myFunc()
