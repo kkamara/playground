148d85fde08305090d30880d427ecc82898f9239
kkamara <kamaracomputers@gmail.com>
2023-07-15 00:49:27 +0100

    Add to learning-folder

---
+++ py/learning-folder/29.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+print('Create account now.')
+username = input('Enter username: ')
+password = input('Enter password: ')
+
+print('Your account has been created successfully.')
+print('Login now.')
+
+username2 = input('Enter username: ')
+password2 = input('Enter password: ')
+
+if username == username2 and password == password2:
+  print('Logged in successfully.')
+else:
+  print('Invalid credentials.')
