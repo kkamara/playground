0afe1083ad03ca091d2c823124ca190f47e21d0d
kkamara <kamaracomputers@gmail.com>
2023-07-15 00:58:43 +0100

    Update app.py

--- py/app.py
+++ py/app.py
@@ -2,6 +2,5 @@
 '''
 https://www.youtube.com/watch?v=jBzwzrDvZ18
 Python Backend Web Development Course (with Django)
-2:45:36 - Try Except in Python, Reading / Writing Files,
-          classes, inheritance and the shell.
+03:38:28 - Django.
 '''
