0dee3b5608a4102d346f921c10956049e8c7d1ae
kkamara <kamaracomputers@gmail.com>
2023-07-29 15:56:28 +0100

    Add to learning-folder

---
+++ c#/learning-folder/6/Program.cs
@@ -0,0 +1,5 @@
+﻿double a = 5;
+double b = 4;
+double c = 2;
+double d = (a + b) / c;
+Console.WriteLine(d);
---
+++ c#/learning-folder/7/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine();
+// dotnet run --this --that=other
+string[] arguments = Environment.GetCommandLineArgs();
+Console.WriteLine("GetCommandLineArgs: {0}", string.Join(", ", arguments));
+// GetCommandLineArgs: /Users/kel/workspace/playground/c#/learning-folder/7/bin/Debug/net7.0/7.dll, --this, --that=other
