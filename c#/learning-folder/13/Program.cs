7b6af7f43e9a6ac450f8b2d8b6302df44da11c12
kkamara <kamaracomputers@gmail.com>
2023-07-30 13:33:37 +0100

    Update indentation

--- c#/learning-folder/13/Program.cs
+++ c#/learning-folder/13/Program.cs
@@ -3,11 +3,11 @@ int b = 3;
 int c = 4;
 if ((a + b + c > 10) || (a == b))
 {
-    Console.WriteLine("The answer is greater than 10");
-    Console.WriteLine("Or the first number is equal to the second");
+  Console.WriteLine("The answer is greater than 10");
+  Console.WriteLine("Or the first number is equal to the second");
 }
 else
 {
-    Console.WriteLine("The answer is not greater than 10");
-    Console.WriteLine("And the first number is not equal to the second");
+  Console.WriteLine("The answer is not greater than 10");
+  Console.WriteLine("And the first number is not equal to the second");
 }
