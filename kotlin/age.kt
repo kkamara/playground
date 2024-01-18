1030eabbbdc26c7fe0344a23b9ce0542016fa655
kkamara <kamaracomputers@gmail.com>
2024-01-18 14:19:08 +0000

    Add Kotlin code: age.kt

---
+++ kotlin/age.kt
@@ -0,0 +1,13 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+    var age: Int
+    age = 24
+    println(age)
+    age = 35
+    println(age)
+    age = 56
+    println(age)
+}
