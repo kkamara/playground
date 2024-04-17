709478c7b67d1cb8a2446e386d6e2527436e1366
kkamara <kamaracomputers@gmail.com>
2024-04-17 16:03:22 +0100

    Use const

--- javascript/dob/dob.js
+++ javascript/dob/dob.js
@@ -2,9 +2,9 @@ const moment = require("moment");
 /**
  * Date of birth (dob)
  */
-var now = moment().format('MMM DD h:mm A');
+const now = moment().format('MMM DD h:mm A');
 console.log(now);
-var under18 = moment().subtract("17", "years").unix();
+const under18 = moment().subtract("17", "years").unix();
 console.log("under 18 :", under18);
 const over18 = moment().subtract("19", "years").unix();
 console.log("over 18 :", over18);
