715bb9875e29f28736c12ed6779243037cec2268
kkamara <kamaracomputers@gmail.com>
2024-09-10 10:25:17 +0100

    Add project: array_column

---
+++ php/array_column/main.php
@@ -0,0 +1,25 @@
+<?php
+$myA = [
+    "test" => [
+        ["apple" => 1],
+        ["apple" => 2],
+        ["apple" => 3],
+    ]
+];
+
+$myA["test"] = array_column($myA["test"], "apple");
+
+var_dump($myA, $myA["test"][0]);
+/*
+    array(1) {
+        ["test"]=>
+        array(3) {
+            [0]=>
+            int(1)
+            [1]=>
+            int(2)
+            [2]=>
+            int(3)
+        }
+    }
+*/;
