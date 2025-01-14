139c832b3e6696073891f2a919812d7b21f4723a
kkamara <kamaracomputers@gmail.com>
2025-01-14 00:40:45 +0000

    Add project: ListIterator

---
+++ java/ListIterator/src/kelvinkamara/MyClass.java
@@ -0,0 +1,35 @@
+package kelvinkamara;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		ArrayList<String> names = new ArrayList<String>();
+		names.add("Mark");
+		names.add("Tom");
+		names.add("John");
+		names.add("Jack");
+		names.add("Patrick");
+		
+		ListIterator<String> itr = names.listIterator();
+//		Iterate
+		while(itr.hasNext())
+			System.out.println(itr.next());
+		
+		System.out.println("");
+		System.out.println("");
+		
+//		Prints empty string on each line.
+		while(itr.hasNext())
+			System.out.println(itr.next());
+
+		System.out.println("");
+		System.out.println("");
+		
+//		Reverse iteration
+		while(itr.hasPrevious())
+			System.out.println(itr.previous());
+	}
+}
