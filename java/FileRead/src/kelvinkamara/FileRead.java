09c2bb1fffcd2bb24d756d716700ae28515c9c29
kkamara <kamaracomputers@gmail.com>
2025-01-15 18:19:33 +0000

    Add project: FileRead

---
+++ java/FileRead/src/kelvinkamara/FileRead.java
@@ -0,0 +1,27 @@
+package kelvinkamara;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileRead {
+	public static void main(String[] args) {
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader("C:\\Users\\kamar\\workspace\\playground\\java\\FileRead\\fileName.txt"));
+			String line;
+			
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			System.out.println(e);
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+		}
+	}
+}
