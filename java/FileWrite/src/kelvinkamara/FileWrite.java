dbcc03122ed77078b4391718117285341f91111d
kkamara <kamaracomputers@gmail.com>
2025-01-15 19:16:42 +0000

    Refactor project: FileWrite 3

--- java/FileWrite/src/kelvinkamara/FileWrite.java
+++ java/FileWrite/src/kelvinkamara/FileWrite.java
@@ -7,21 +7,17 @@ import java.io.PrintWriter;
 public class FileWrite {
 	public static void main(String[] args) {
 		File file = new File(System.getProperty("user.dir")+"\\java\\FileWrite\\fileName.txt");
-		PrintWriter pw = null;	
-		
-		try {
+
+		try (PrintWriter pw = new PrintWriter(file)) { /* For try-with-resources see https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html */
 			if (!file.exists()) {
 				file.createNewFile();
 			}
 			
-			pw = new PrintWriter(file);
 			pw.println("This is my file content");
 			pw.println(100000);
 			System.out.println("Done");
 		} catch (IOException e) {
 			System.out.println(e);
-		} finally {
-			pw.close();
 		}
 	}
 }
