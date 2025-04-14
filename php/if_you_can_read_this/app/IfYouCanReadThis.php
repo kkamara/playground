b8b1c96d7704fc1b7796a01e59ea5817a99090c0
kkamara <kamaracomputers@gmail.com>
2025-04-14 23:37:47 +0100

    Fix class function visibility modifier

--- php/if_you_can_read_this/app/IfYouCanReadThis.php
+++ php/if_you_can_read_this/app/IfYouCanReadThis.php
@@ -8,7 +8,7 @@ namespace App;
  */
 class IfYouCanReadThis
 {
-    static function execute(string $string)
+    public static function execute(string $string)
     {
         $commands = ['a' => 'Alfa', 'b' => 'Bravo', 'c' => 'Charlie', 'd' => 'Delta', 'e' => 'Echo', 'f' => 'Foxtrot', 'g' => 'Golf', 'h' => 'Hotel', 'i' => 'India', 'j' => 'Juliett', 'k' => 'Kilo', 'l' => 'Lima', 'm' => 'Mike', 'n' => 'November', 'o' => 'Oscar', 'p' => 'Papa', 'q' => 'Quebec', 'r' => 'Romeo', 's' => 'Sierra', 't' => 'Tango', 'u' => 'Uniform', 'v' => 'Victor', 'w' => 'Whiskey', 'x' => 'X-ray', 'y' => 'Yankee', 'z' => 'Zulu',];
         $in = str_split(strtolower($string));
