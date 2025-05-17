13b5e1899cb6837c5307bf983803c7b843d5e397
kkamara <kamaracomputers@gmail.com>
2025-05-17 02:07:23 +0100

    Add comment to MySQL script

--- sql/mysql/updating-optional-parameters.sql
+++ sql/mysql/updating-optional-parameters.sql
@@ -1,3 +1,5 @@
+-- This is useful for PATCH requests, where only some
+-- items are provided for update.
 UPDATE userTokens
 SET authToken = COALESCE("1", authToken), createdAt = COALESCE(null, createdAt) 
 WHERE id = 1;
