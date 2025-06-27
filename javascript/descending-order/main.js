dba00ed3b865e1164c2a89b53f640fb2a8d76555
kkamara <kamaracomputers@gmail.com>
2025-06-27 21:58:59 +0100

    Update comment for readability

--- javascript/descending-order/main.js
+++ javascript/descending-order/main.js
@@ -1,5 +1,8 @@
 /*
- *  Your task is to make a function that can take any non-negative integer as a argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+ *  Your task is to make a function that can take any non-negative
+ *  integer as a argument and return it with its digits in descending
+ *  order. Essentially, rearrange the digits to create the highest
+ *  possible number.
  *  Examples:
  *      Input: 21445 Output: 54421
  *      Input: 145263 Output: 654321
