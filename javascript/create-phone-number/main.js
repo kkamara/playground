cb86924e862c0531e4665e9a3401e2875edc073e
kkamara <kamaracomputers@gmail.com>
2025-06-27 21:43:05 +0100

    Add project: create-phone-number

---
+++ javascript/create-phone-number/main.js
@@ -0,0 +1,26 @@
+/*
+  Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+  Example
+  createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+  The returned format must be correct in order to complete this challenge.
+
+  Don't forget the space after the closing parentheses!
+*/
+
+/**
+ *
+ * @param {array} numbers
+ * @return string
+ */
+function createPhoneNumber(numbers){
+  let result = "";
+  numbers = numbers.join("");
+  result += "("+numbers.slice(0, 3)+") ";
+  result += numbers.slice(3, 6);
+  result += "-";
+  result += numbers.slice(6);
+  return result;
+}
+
+module.exports = { createPhoneNumber, };
---
+++ javascript/create-phone-number/test.spec.js
@@ -0,0 +1,19 @@
+require("chai").should();
+require("mocha");
+
+const { createPhoneNumber, } = require("./main");
+
+let res;
+
+describe("Main test:", () => {
+  it("Fixed tests", () => {
+    res = createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+    res.should.equal("(123) 456-7890");
+
+    res = createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+    res.should.equal("(111) 111-1111");
+
+    res = createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+    res.should.equal("(123) 456-7890");
+  });
+});
