6d66a9b4b3465ae5ef180bf5243c5aafa63f69d5
kkamara <kamaracomputers@gmail.com>
2025-06-27 22:02:05 +0100

    Add project: credit-card-mask

---
+++ javascript/credit-card-mask/main.js
@@ -0,0 +1,40 @@
+/*
+  Usually when you buy something, you're asked whether your credit
+  card number, phone number or answer to your most secret question
+  is still correct. However, since someone could look over your
+  shoulder, you don't want that shown on your screen. Instead, we
+  mask it.
+
+  Your task is to write a function maskify, which changes all but
+  the last four characters into '#'.
+
+  Examples (input --> output):
+  "4556364607935616" --> "############5616"
+      "64607935616" -->      "#######5616"
+                "1" -->                "1"
+                  "" -->                 ""
+
+  // "What was the name of your first pet?"
+  "Skippy" --> "##ippy"
+  "Nananananananananananananananana Batman!" --> "####################################man!"
+*/
+
+/**
+ *
+ * @param {string} cc
+ * @return string
+ */
+function maskify(cc) {
+  if (4 >= cc.length) {
+    return cc;
+  }
+  let result = "";
+  const limit = cc.length - 4;
+  for (let i = 0; i < limit; i++) {
+    result += "#";
+  }
+  result += cc.slice(-4)
+  return result;
+}
+
+module.exports = { maskify, };
---
+++ javascript/credit-card-mask/test.spec.js
@@ -0,0 +1,19 @@
+require("chai").should();
+require("mocha");
+
+const { maskify, } = require("./main");
+
+let res;
+
+describe("Main tests:", () => {
+  it("should work for some examples", () => {
+    res = maskify("4556364607935616");
+    res.should.equal("############5616");
+
+    res = maskify("1");
+    res.should.equal("1");
+
+    res = maskify("11111");
+    res.should.equal("#1111");
+  });
+});
