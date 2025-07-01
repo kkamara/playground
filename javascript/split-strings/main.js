8dd768b2e46d22394c84c3df7ed8222d099c2024
kkamara <kamaracomputers@gmail.com>
2025-07-01 01:25:49 +0100

    Update comment for readability

--- javascript/split-strings/main.js
+++ javascript/split-strings/main.js
@@ -1,5 +1,7 @@
 /*
-  Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+  Complete the solution so that it splits the string into pairs of two characters.
+  If the string contains an odd number of characters then it should replace the
+  missing second character of the final pair with an underscore ('_').
 
   Examples:
 
