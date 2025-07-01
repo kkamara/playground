85902635e2985f1d2e4721b78221f0b84c043378
kkamara <kamaracomputers@gmail.com>
2025-07-01 01:28:06 +0100

    Add project: detect-pangram

---
+++ javascript/detect-pangram/main.js
@@ -0,0 +1,25 @@
+/*
+  A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+  Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+*/
+
+/**
+ *
+ * @param {string} string
+ * @return boolean
+ */
+function isPangram(string){
+  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let result = true;
+  const lowerCaseInput = string.toLowerCase();
+  for (const char of alphabet) {
+    if (false === lowerCaseInput.includes(char)) {
+      result = false;
+      break;
+    }
+  }
+  return result;
+}
+
+module.exports = { isPangram, };
---
+++ javascript/detect-pangram/test.spec.js
@@ -0,0 +1,15 @@
+const chai = require("chai");
+const { isPangram, } = require("./main");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Tests", () => {
+  it("test1", () => {
+    var string = "The quick brown fox jumps over the lazy dog."
+    assert.strictEqual(isPangram(string), true)
+  })
+  it("test2", () => {
+    var string = "This is not a pangram."
+    assert.strictEqual(isPangram(string), false)
+  });
+});
