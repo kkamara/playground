d38f2a1e58bac68a3ea6a3080c80926116348066
kkamara <kamaracomputers@gmail.com>
2025-07-03 18:35:39 +0100

    Add project: return-negative

---
+++ javascript/return-negative/test.spec.js
@@ -0,0 +1,8 @@
+const { assert, } = require('chai');
+const { makeNegative, } = require("./main");
+
+describe("Tests", () => {
+  it("test", () => {
+     assert.strictEqual(makeNegative(42), -42);
+  });
+});
