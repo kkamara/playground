8c19114445beef87a66213d1eaacd370f84aab10
kkamara <kamaracomputers@gmail.com>
2025-07-03 18:30:41 +0100

    Add project: convert-boolean-values-to-strings-yes-or-no

---
+++ javascript/convert-boolean-values-to-strings-yes-or-no/main.js
@@ -0,0 +1,21 @@
+/*
+  Complete the method that takes a boolean value and return a
+  "Yes" string for true, or a "No" string for false.
+*/
+
+/**
+ * @param {boolean} n
+ * @return string
+ */
+function boolToWord( bool ){
+  switch(bool) {
+    case true:
+      return "Yes";
+    break;
+    case false:
+      return "No";
+    break;
+  }
+}
+
+module.exports = { boolToWord, };
