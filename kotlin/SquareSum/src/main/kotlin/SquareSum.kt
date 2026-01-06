7f7b85d3fb15988dfd8a28f593354b062da7bced
kkamara <kamaracomputers@gmail.com>
2026-01-06 19:07:02 +0000

    Add challenge details to SquareSum project

--- kotlin/SquareSum/src/main/kotlin/SquareSum.kt
+++ kotlin/SquareSum/src/main/kotlin/SquareSum.kt
@@ -1,5 +1,14 @@
 package solution
 
+/*
+    Complete the square sum function so that it squares
+    each number passed into it and then sums the results
+    together.
+
+    For example, for [1, 2, 2] it should return 9 because
+    1^2 + 2^2 + 2^2 = 9 .
+ */
+
 fun squareSum(n: Array<Int>): Int {
     var result = 0
     for (i in n) {
