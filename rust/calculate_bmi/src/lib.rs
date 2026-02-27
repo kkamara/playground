3648586155f1984599ac600b4acb0ca9c6ae0614
kkamara <kamaracomputers@gmail.com>
2026-02-27 17:34:58 +0000

    Refactor calculate_bmi

--- rust/calculate_bmi/src/lib.rs
+++ rust/calculate_bmi/src/lib.rs
@@ -11,20 +11,17 @@
 /// if bmi > 30 return "Obese"
 
 pub fn bmi(weight: u32, height: f32) -> &'static str {
-    let bmi = (weight as f32) / (height * height);
-    if 18.5 >= bmi {
-        return "Underweight";
+    let bmi = weight as f32 / (height * height);
+
+    if bmi <= 18.5 {
+        "Underweight"
+    } else if bmi <= 25.0 {
+        "Normal"
+    } else if bmi <= 30.0 {
+        "Overweight"
+    } else {
+        "Obese"
     }
-    if 25.0 >= bmi {
-        return "Normal";
-    }
-    if 30.0 >= bmi {
-        return "Overweight";
-    }
-    if 30.0 < bmi {
-        return "Obese";
-    }
-    ""
 }
 
 #[cfg(test)]
