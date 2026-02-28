565c48d5746fb879b31b2890bab212c570f40c29
kkamara <kamaracomputers@gmail.com>
2026-02-28 02:58:11 +0000

    Add project: sum_of_positive

---
+++ rust/sum_of_positive/src/lib.rs
@@ -0,0 +1,45 @@
+/// Sum of Positive
+///
+/// Task
+/// 
+/// You get an array of numbers, return the sum of all of the positives ones.
+///
+/// Example
+/// 
+/// [1, -4, 7, 12] => 1 + 7 + 12 = 20
+/// 
+/// Note
+/// 
+/// If there is nothing to sum, the sum is default to 0. 
+
+pub fn positive_sum(slice: &[i32]) -> i32 {
+    let mut res = 0;
+    for num in slice {
+        if &0 < num {
+            res += num
+        }
+    }
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn some_examples() {
+        assert_eq!(positive_sum(&[1,2,3,4,5]), 15);
+        assert_eq!(positive_sum(&[1,-2,3,4,5]), 13);
+        assert_eq!(positive_sum(&[-1,2,3,4,-5]), 9);
+    }
+    
+    #[test]
+    fn empty_list() {
+        assert_eq!(positive_sum(&[]), 0);
+    }
+    
+    #[test]
+    fn all_negative() {
+        assert_eq!(positive_sum(&[-1,-2,-3,-4,-5]), 0);
+    }       
+}
