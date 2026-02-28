29135bfb1f6a39d2e55178884579673c518bf464
kkamara <kamaracomputers@gmail.com>
2026-02-28 02:48:07 +0000

    Add project: volume_of_a_cuboid

---
+++ rust/volume_of_a_cuboid/src/lib.rs
@@ -0,0 +1,20 @@
+/// Volume of a Cuboid
+/// 
+/// Bob needs a fast way to calculate the volume of a rectangular cuboid with three values: the length, width and height of the cuboid.
+///
+/// Write a function to help Bob with this calculation.
+
+pub fn get_volume_of_cuboid(length: f64, width: f64, height: f64) -> f64 {
+    length * width * height
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(get_volume_of_cuboid(1.0, 2.0, 2.0), 4.0);
+        assert_eq!(get_volume_of_cuboid(6.3, 2.0, 5.0), 63.0);
+    }
+}
