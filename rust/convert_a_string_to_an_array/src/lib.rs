fd95cb869f39199bb050b0ee3cdbd4e78be519b9
kkamara <kamaracomputers@gmail.com>
2026-02-28 02:44:11 +0000

    Add project: convert_a_string_to_an_array

---
+++ rust/convert_a_string_to_an_array/src/lib.rs
@@ -0,0 +1,33 @@
+/// Convert a String to an Array
+/// 
+/// Write a function to split a string and convert it into an array of words.
+///
+/// Examples (Input ==> Output):
+/// 
+/// "Robin Singh" ==> ["Robin", "Singh"]
+///
+/// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+pub fn string_to_array(s: &str) -> Vec<String> {
+    s.split(" ")
+        .map(String::from)
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::string_to_array;
+
+    fn dotest(s: &str, expected: &[&str]) {
+        let actual = string_to_array(s);
+        assert!(actual == expected, "Test failed with s = \"{s}\"\nExpected {expected:?} but got {actual:?}")
+    }
+
+    #[test]
+    fn fixed_tests() {
+        dotest("Robin Singh", &["Robin", "Singh"]);
+        dotest("CodeWars", &["CodeWars"]);
+        dotest("I love arrays they are my favorite", &["I", "love", "arrays", "they", "are", "my", "favorite"]);
+        dotest("1 2 3", &["1", "2", "3"]);
+    }
+}
