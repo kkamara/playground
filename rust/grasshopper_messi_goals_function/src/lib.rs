5bf4e459a1b38dc28a756497c9aa87b2801bc913
kkamara <kamaracomputers@gmail.com>
2026-02-28 03:54:17 +0000

    Add project: grasshopper_messi_goals_function

---
+++ rust/grasshopper_messi_goals_function/src/lib.rs
@@ -0,0 +1,40 @@
+//! Grasshopper - Messi goals function
+
+/// Messi goals function
+///
+/// Messi is a soccer player with goals in three leagues:
+///
+/// LaLiga
+/// 
+/// Copa del Rey
+/// 
+/// Champions
+/// 
+/// Complete the function to return his total number of goals in all three leagues.
+///
+/// Note: the input will always be valid.
+///
+/// For example:
+///
+/// 5, 10, 2  -->  17
+
+pub fn goals(la_liga_goals: i32, champions_league_goals: i32, copa_del_rey_goals: i32) -> i32 {
+   la_liga_goals + champions_league_goals + copa_del_rey_goals
+}
+
+#[cfg(test)]
+mod tests {
+    use super::goals;
+    
+    fn dotest(a: i32, b: i32, c: i32, expected: i32) {
+        let actual = goals(a, b, c);
+        assert!(actual == expected, 
+            "With la_liga_goals = {a}, champions_league_goals = {b}, copa_del_rey_goals = {c}\nExpected {expected} but got {actual}")
+    }
+    
+    #[test]
+    fn test_goals() {
+        dotest(0, 0, 0, 0);
+        dotest(43, 10, 5, 58);
+    }
+}
