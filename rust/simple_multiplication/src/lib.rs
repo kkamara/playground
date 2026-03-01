53db592edde3d232a96dbddeac9f1adfdb336746
kkamara <kamaracomputers@gmail.com>
2026-03-01 00:12:43 +0000

    Add project: simple_multiplication

---
+++ rust/simple_multiplication/src/lib.rs
@@ -0,0 +1,23 @@
+//! Simple Multiplication
+
+/// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+pub fn simple_multiplication(number: u8) -> u8 {
+    match number % 2 {
+        0 => 8 * number,
+        _ => 9 * number,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_basic() {
+        assert_eq!(simple_multiplication(1), 9);
+        assert_eq!(simple_multiplication(2), 16);
+        assert_eq!(simple_multiplication(4), 32);
+        assert_eq!(simple_multiplication(5), 45);
+    }
+}
