20cb6fe9ff77daf28fdd21c882bf69aeba349ec7
kkamara <kamaracomputers@gmail.com>
2026-03-16 20:20:13 +0000

    Add project: How good are you really?

---
+++ typescript/how_good_are_you_really/main.ts
@@ -0,0 +1,28 @@
+/*
+  How good are you really?
+  https://www.codewars.com/kata/5601409514fc93442500010b
+
+  There was a test in your class and you passed it. Congratulations!
+
+  But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+  You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+  Return true if you're better, else false!
+
+  Note:
+    Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
+*/
+
+/**
+ * @param {number[]} classPoints
+ * @param {number} yourPoints
+ * @returns {boolean}
+ */
+export default function betterThanAverage(classPoints: number[], yourPoints: number) : boolean {
+  let sum = yourPoints;
+  for (const num of classPoints) {
+    sum += num;
+  }
+  return (sum / (classPoints.length + 1)) < yourPoints;
+}
